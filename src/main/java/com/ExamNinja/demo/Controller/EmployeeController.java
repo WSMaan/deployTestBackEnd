3e3a7539a70d3df922cfe27a735a47af9747624f
WSMaan <118893766+WSMaan@users.noreply.github.com>
2025-01-16 16:08:12 +0000

    Update EmployeeController.java

--- src/main/java/com/ExamNinja/demo/Controller/EmployeeController.java
+++ src/main/java/com/ExamNinja/demo/Controller/EmployeeController.java
@@ -1,23 +1,26 @@
 package com.ExamNinja.demo.Controller;
 
 import com.ExamNinja.demo.model.Employee;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 public class EmployeeController {
 
     private List<Employee> employees = createList();
 
-    @RequestMapping(value = "/employees", method = RequestMethod.GET,
-            produces = "application/json")
+    @RequestMapping(value = "/employees", method = RequestMethod.GET, produces = "application/json")
     public List<Employee> getEmployees() {
         return employees;
+    }
+
+    @RequestMapping(value = "/employees", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public Employee addEmployee(@RequestBody Employee newEmployee) {
+        employees.add(newEmployee);
+        return newEmployee;
     }
 
     private static List<Employee> createList() {
@@ -32,6 +35,7 @@ public class EmployeeController {
         emp2.setName("emp2");
         emp2.setId(2);
         emp2.setDepartment("Finance");
+
         Employee emp3 = new Employee();
         emp3.setName("abc");
         emp3.setId(3);
@@ -43,5 +47,4 @@ public class EmployeeController {
 
         return tempEmployees;
     }
-
 }
